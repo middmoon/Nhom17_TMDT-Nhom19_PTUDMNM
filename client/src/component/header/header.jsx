a25da10bdef4da8c21d0617b41cc20ef0758dd79
Yung Ah <pumpo6969@gmail.com>
2024-08-29 18:39:48 +0700

    test

--- client/src/component/header/header.jsx
+++ client/src/component/header/header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../header/header.css";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import Logo from "../../assets/images/Logo.svg";
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Select from "../selectDrop/select";
@@ -9,12 +11,12 @@ const Header = () => {
     <>
       <header>
         <div className="container-fluid">
-          <div className="row">
-            <div className=" col-md-3 col-lg-2">
+          <div className="row" >
+            <div className=" col-lg-2 pb-3">
               <img src={Logo} alt="" />
             </div>
             {/* Header */}
-            <div className="col-md-4 col-lg-5">
+            <div className=" col-lg-5 pb-3">
               <div className="headerSearch ">
                 <Select />
 
@@ -28,6 +30,29 @@ const Header = () => {
               </div>
             </div>
             {/* Header */}
+
+           <div className="col-lg-5 pb-3">
+           <div className="loginNav">
+              <NavDropdown
+                className="navButton"
+                title={
+                  <>
+                    <FontAwesomeIcon icon={faUserPlus} />{'  Login'} 
+                  </>
+                }
+                id="basic-nav-dropdown"
+              >
+                <div className="navbtItems">
+                  <NavDropdown.Item href="/User/order">Order</NavDropdown.Item>
+                  <NavDropdown.Item href="">Setting</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item >
+                    log out
+                  </NavDropdown.Item>
+                </div>
+              </NavDropdown>
+            </div>
+           </div>
           </div>
         </div>
       </header>
--- client/src/component/selectDrop/select.js
+++ client/src/component/selectDrop/select.js
@@ -5,19 +5,21 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 const Select = () => {
   const [isOpenSelect, setisOpenSelect] = useState(false);
   const [selectedIndex, setselectedIndex] = useState(0);
+  const [selectedItem, setselectedItem] = useState("Tất cả");
   const openSelect = () => {
     setisOpenSelect(!isOpenSelect);
   };
 
-  const closeSelect = (index) => {
+  const closeSelect = (index,name) => {
     setselectedIndex(index);
     setisOpenSelect(false);
+    setselectedItem(name);
   };
   return (
     <>
       <div className="selectDrop">
         <span className="openSelect" onClick={openSelect}>
-          Tất cả{" "}
+          {selectedItem}{" "}
           <FontAwesomeIcon icon={faCaretDown} style={{ padding: " 0px 5px" }} />
         </span>
         {isOpenSelect === true && (
@@ -27,49 +29,49 @@ const Select = () => {
             </div>
             <ul className="searchResults">
               <li
-                onClick={() => closeSelect(0)}
+                onClick={() => closeSelect(0,'Tất cả')}
                 className={`${selectedIndex === 0 ? `active` : ``}`}
               >
                 Tất cả
               </li>
               <li
-                onClick={() => closeSelect(1)}
+                onClick={() => closeSelect(1,'Điện thoại')}
                 className={`${selectedIndex === 1 ? `active` : ``}`}
               >
                 Điện thoại
               </li>
               <li
-                onClick={() => closeSelect(2)}
+                onClick={() => closeSelect(2,'Thời trang')}
                 className={`${selectedIndex === 2 ? `active` : ``}`}
               >
                 Thời trang
               </li>
               <li
-                onClick={() => closeSelect(3)}
+                onClick={() => closeSelect(3,'Thiết bị điện tử')}
                 className={`${selectedIndex === 3 ? `active` : ``}`}
               >
                 Thiết bị điện tử
               </li>
               <li
-                onClick={() => closeSelect(4)}
+                onClick={() => closeSelect(4,'Thú cưng')}
                 className={`${selectedIndex === 4 ? `active` : ``}`}
               >
                 Thú cưng
               </li>
               <li
-                onClick={() => closeSelect(5)}
+                onClick={() => closeSelect(5,'Nội thất')}
                 className={`${selectedIndex === 5 ? `active` : ``}`}
               >
                 Nội thất
               </li>
               <li
-                onClick={() => closeSelect(6)}
+                onClick={() => closeSelect(6,'Chăm sóc sắc đẹp')}
                 className={`${selectedIndex === 6 ? `active` : ``}`}
               >
                 Chăm sóc sắc đẹp
               </li>
               <li
-                onClick={() => closeSelect(7)}
+                onClick={() => closeSelect(7,'Thực phẩm')}
                 className={`${selectedIndex === 0 ? "active" : ""}`}
               >
                 Thực phẩm
